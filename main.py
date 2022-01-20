2be5d5d617da0fcec529a8ab058296fe041f0c2a
morgadodesarrollador <34490065+morgadodesarrollador@users.noreply.github.com>
2022-01-20 19:27:33 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.py
@@ -0,0 +1,8 @@
+from microbit import *
+
+while True:
+    basic.show_string("Hello!")
+    basic.pause(1000)
+    basic.show_icon(IconNames.HEART)
+    basic.pause(1000)
+
---
+++ main.ts
@@ -0,0 +1,6 @@
+while (true) {
+    basic.showString("Hello!")
+    basic.pause(1000)
+    basic.showIcon(IconNames.Heart)
+    basic.pause(1000)
+}
